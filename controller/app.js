34239d5df174cf9541cff4f021a3f7f545751b12
ibelieveolive <114415281+ibelieveolive@users.noreply.github.com>
2022-10-03 14:29:00 +0200

    new app

--- controller/app.js
+++ controller/app.js
@@ -2,24 +2,24 @@ const knex = require("../model/knex.js")
 
 
 
-function getAllPersos(){
-    return knex('persojjk').select('*')
+async function getAllPersos(){
+    return await knex('persojjk').select('*')
 }
 
-function getOnePerso(id){
-    return knex("persojjk").where("id", id)
+async function getOnePerso(id){
+    return await knex("persojjk").where("id", id)
 }
 
-function postPerso(perso){
-    return knex("persojjk").insert(perso)
+async function postPerso(perso){
+    return await knex("persojjk").insert(perso)
 }
 
-function updatePerso(id, perso){
-    return knex("persojjk").where("id", id).update(perso)
+async function updatePerso(id, perso){
+    return await knex("persojjk").where("id", id).update(perso)
 }
 
-function deletePerso(id){
-    return knex("persojjk").where("id", id).del()
+async function deletePerso(id){
+    return await knex("persojjk").where("id", id).del()
 }
 
 
