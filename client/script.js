2878c04ea366f5b1858db47829275d07a93b54f1
ibelieveolive <114415281+ibelieveolive@users.noreply.github.com>
2022-10-03 16:06:28 +0200

    last commit

---
+++ client/script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+
+fetch('https://fine-outfit-pike.cyclic.app/')
+.then(res => res.json())
+.then(code => displayPerso(code))
+
+
+const body = document.body
+
+function displayPerso(perso) {
+    
+
+    const div = document.createElement('div')
+    body.appendChild(div)
+    div.classList.add('container')
+    const container = document.querySelector('.container')
+    perso.forEach(perso => {
+        const ul = document.createElement('ul')
+        container.appendChild(ul)
+        ul.innerHTML += `<li> 
+                            Nom : ${perso.lastname} 
+                            <br>  Prénom : ${perso.firstname} 
+                            <br>  Age : ${perso.age} 
+                            <br>  Pouvoir : ${perso.power} 
+                        </li>`
+    });
+   
+    
+    
+
+}
